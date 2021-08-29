45c243d7b359d622c51464ce7b08cfd41d2ecd61
Nisha <nisha@zipcodes-mbp-8.myfiosgateway.com>
2021-08-29 16:11:49 -0400

    Done BlogPostServiceTest

--- src/test/java/com/example/demo/service/BlogPostServiceTest.java
+++ src/test/java/com/example/demo/service/BlogPostServiceTest.java
@@ -1,25 +1,18 @@
 package com.example.demo.service;
 
 import com.example.demo.models.BlogPost;
-import com.example.demo.models.Recipe;
 import com.example.demo.models.Tag;
-import com.example.demo.models.User;
 import com.example.demo.repository.BlogPostRepository;
 import com.example.demo.repository.TagRepository;
-
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
@@ -34,6 +27,7 @@ class BlogPostServiceTest {
 
     @Mock
     TagRepository tagRepository;
+
 
 
     @BeforeEach
@@ -57,7 +51,7 @@ class BlogPostServiceTest {
     }
 
     @Test
-    void read()  {
+    void readTest()  {
         //given
         List<Long> id = new ArrayList<>();
         BlogPost blogPost = new BlogPost();
@@ -66,6 +60,7 @@ class BlogPostServiceTest {
         //when
         when(repository.findById(anyLong())).thenReturn(Optional.of(expected));
         when(repository.findTagIdsByBlog(blogPost.getId())).thenReturn(id);
+        when(tagRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(new Tag()));
         BlogPost Actual = blogPostService.read(anyLong());
 
         //Then
@@ -78,11 +73,13 @@ class BlogPostServiceTest {
         BlogPost blogPost = new BlogPost();
         List<BlogPost> result = new ArrayList<>();
         result.add(new BlogPost());
+        List<Long> tagIds = new ArrayList<>();
         List expected = new ArrayList();
 
 
         //when
         when(repository.findAll()).thenReturn(expected);
+        when(repository.findTagIdsByBlog(blogPost.getId())).thenReturn(tagIds);
 
         List Actual = blogPostService.readAll();
 
@@ -93,15 +90,57 @@ class BlogPostServiceTest {
 
     @Test
     void update() {
+        //given
+        List<Long> id = new ArrayList<>();
+        id.add(5L);
+        id.add(6L);
+        BlogPost newBlogPost = new BlogPost();
+        newBlogPost.setImageUrl("abc.com");
+        newBlogPost.setTitle("My Blog");
+        newBlogPost.setContent(null);
+
+
+        //when
+        when(repository.findById(anyLong())).thenReturn(Optional.of(newBlogPost));
+        when(repository.findTagIdsByBlog(newBlogPost.getId())).thenReturn(id);
+        when(tagRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(new Tag()));
+        when(repository.save(newBlogPost)).thenReturn(newBlogPost);
+
+        BlogPost Actual = blogPostService.update(Mockito.anyLong(),newBlogPost);
+
+        //Then
+        assertEquals(newBlogPost,Actual);
 
     }
 
     @Test
     void delete() {
+        //given
+        BlogPost blogPost = new BlogPost();
+
+        //when
+        Mockito.when(repository.findById(2L)).thenReturn(Optional.of(blogPost));
+        BlogPost Actual = blogPostService.delete(blogPost);
+
+        //Then
+        assertEquals(blogPost,Actual);
+
     }
 
     @Test
     void testDelete() {
+        //given
+        BlogPost blogPost = new BlogPost();
+
+        //when
+        Mockito.when(repository.findById(4L)).thenReturn(Optional.of(blogPost));
+        BlogPost Actual = blogPostService.delete(4L);
+
+        //Then
+        assertEquals(blogPost,Actual);
+
+
+
 
 
 
