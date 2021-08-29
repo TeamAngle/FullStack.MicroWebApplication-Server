ade3f6a6475291778c452610da6601d723b7dc22
Nisha <nisha@zipcodes-mbp-8.myfiosgateway.com>
2021-08-29 17:55:51 -0400

    recipe service test done

--- src/test/java/com/example/demo/service/RecipeServiceTest.java
+++ src/test/java/com/example/demo/service/RecipeServiceTest.java
@@ -10,6 +10,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RecipeServiceTest {
@@ -22,31 +26,95 @@ class RecipeServiceTest {
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(true);
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     void create() {
+        //given
+        Recipe recipe = new Recipe();
+        Recipe expected = new Recipe();
 
+        //when
+        Mockito.when(repository.save(recipe)).thenReturn(expected);
+        Recipe Actual = recipeService.create(recipe);
+
+        //Then
+        assertEquals(expected,Actual);
     }
 
     @Test
-    void read() {
+    void readTest() {
+        //given
+        List<Long> id = new ArrayList<>();
+        Recipe expected = new Recipe();
+
+        //when
+        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(java.util.Optional.of(expected));
+        Recipe Actual = recipeService.read(Mockito.anyLong());
+
+        //Then
+        assertEquals(expected,Actual);
     }
 
     @Test
     void readAll() {
+        //given
+        List<Recipe> list = new ArrayList<>();
+        list.add(new Recipe());
+        List expected = new ArrayList();
+
+        //when
+        Mockito.when(repository.findAll()).thenReturn(expected);
+        List Actual = recipeService.readAll();
+
+        //Then
+        assertEquals(expected,Actual);
     }
 
     @Test
     void update() {
+        //given
+        Recipe recipe = new Recipe();
+        recipe.setName("Airfryer");
+        recipe.setIngredients("Spices");
+        recipe.setInstructions("Follow me");
+        List<Long> id = new ArrayList<>();
+        id.add(5L);
+        id.add(6L);
+
+        //when
+        Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(recipe));
+        Mockito.when(repository.save(recipe)).thenReturn(recipe);
+        Recipe Actual = recipeService.update(Mockito.anyLong(),recipe);
+
+        //Then
+        assertEquals(recipe,Actual);
     }
 
     @Test
     void delete() {
+        //given
+        Recipe recipe = new Recipe();
+
+        //when
+        Mockito.when(repository.findById(2L)).thenReturn(Optional.of(recipe));
+        Recipe Actual = recipeService.delete(recipe);
+
+        //Then
+        assertEquals(recipe,Actual);
     }
 
     @Test
     void testDelete() {
+        //given
+        Recipe recipe = new Recipe();
+
+        //when
+        Mockito.when(repository.findById(4L)).thenReturn(Optional.of(recipe));
+        Recipe Actual = recipeService.delete(4L);
+
+        //Then
+        assertEquals(recipe,Actual);
     }
 }
