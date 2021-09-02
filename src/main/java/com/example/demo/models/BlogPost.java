1a965c7607aa1bfe709d600aa253d627ebfcfdb1
jgiroso <jrgiroso@gmail.com>
2021-09-02 14:15:46 -0400

    readjusted blogpost class

--- src/main/java/com/example/demo/models/BlogPost.java
+++ src/main/java/com/example/demo/models/BlogPost.java
@@ -2,6 +2,7 @@ package com.example.demo.models;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,17 +20,27 @@ public class BlogPost {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "blogpost_id")
     private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "imageurl")
     private String imageUrl;
+    @Column(name = "ingredients")
+    private String ingredients;
+    @Column(name = "instructions")
+    private String instructions;
+    @Column(name = "content")
     private Clob content;
-    @OneToOne(cascade = CascadeType.ALL)
-    private Recipe recipe;
     @ManyToOne
-    @JsonIgnore
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+//    @JsonIgnore
     private User user;
     @ManyToMany
-    @JsonIgnore
+    @JoinTable(name="blogpost_tag",
+        joinColumns = @JoinColumn(name = "blog_id", referencedColumnName = "blogpost_id"),
+        inverseJoinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id"))
+    @JsonIgnoreProperties("blogPosts")
     private List<Tag> tags;
 
 
