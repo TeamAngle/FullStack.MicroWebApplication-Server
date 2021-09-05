bb90cb4cf99ff3de57a95fd6a4d537af92246bab
jgiroso <jrgiroso@gmail.com>
2021-09-05 15:12:31 -0400

    lombok successful

--- src/main/java/com/example/demo/models/Tag.java
+++ src/main/java/com/example/demo/models/Tag.java
@@ -1,11 +1,17 @@
 package com.example.demo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tags")
 public class Tag {
     @Id
@@ -19,34 +25,6 @@ public class Tag {
     @JsonIgnoreProperties("tags")
     List<BlogPost> blogPosts;
 
-    public Tag() {
-    }
-
-    public Tag(Long id, String name, List<BlogPost> blogPosts) {
-        this.id = id;
-        this.name = name;
-        this.blogPosts = blogPosts;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<BlogPost> getBlogPosts() { return blogPosts; }
-
-    public void setBlogPosts(List<BlogPost> blogPosts) { this.blogPosts = blogPosts; }
 }
 
 
