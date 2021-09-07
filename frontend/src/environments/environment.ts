960469f8824a1bc29a20ac354d345ce60dd71ee9
jgiroso <jrgiroso@gmail.com>
2021-09-07 19:12:30 -0400

    heroku test 5

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080'
-//   apiBaseUrl: 'http://https://sublime-kitchen-backend.herokuapp.com'
+//   apiBaseUrl: 'http://localhost:8080'
+  apiBaseUrl: 'http://https://sublime-kitchen-backend.herokuapp.com'
 
 };
 
